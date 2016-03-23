7c4e903d24ff58899265c5c3ef02f8ea52bfa75b
Aundra Miller <miller.aundra@gmail.com>
2016-03-23 15:38:37 -0700

    create edit button and keg-edit component

--- app/keg-details.component.ts
+++ app/keg-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Keg } from './keg.model';
+import { KegEditComponent } from './keg-edit.component';
 
 @Component ({
   selector: 'keg-details',
@@ -8,18 +9,34 @@ import { Keg } from './keg.model';
     <h3>Keg Name: {{ keg.name }}</h3>
     <h4>Percentage: {{ keg.percentage }}</h4>
     <h4>Pints Remaining: {{ keg.pints }}</h4>
-    <button (click)="kegReduced(keg)">Reduce Keg</button>
+    <button
+      (click)="kegReduced(keg)"
+      class="btn btn-md btn-danger">
+    Reduce Keg</button>
+    <button
+      *ngIf="!showEdit" (click)="toggleEdit(true)"
+      class="btn btn-md btn-warning">
+    Edit Description</button>
+    <keg-edit
+      [keg]="keg">
+    </keg-edit>
   `
 })
 
 export class KegDetailsComponent {
   public reducePintCount: EventEmitter<Keg>;
+  public showEdit: boolean;
   constructor() {
     this.reducePintCount = new EventEmitter();
+    this.showEdit = false;
   }
 
   kegReduced(chosenKeg: Keg): void {
     chosenKeg.pints = chosenKeg.pints - 1;
     console.log('trying to reduce keg' + chosenKeg.pints);
+  }
+
+  toggleEdit(editDisplayStatus: boolean) {
+    this.showEdit = editDisplayStatus;
   }
 }
---
+++ app/keg-edit.component.ts
@@ -0,0 +1,3 @@
+import { Component } from 'angular2/core';
+import { Keg } from './keg.model';
+import { KegDetailsComponent } from './keg-details.component';
