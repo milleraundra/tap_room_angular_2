0ec986bb8f7547cc4c5803704218062ba40c91fa
Aundra Miller <miller.aundra@gmail.com>
2016-03-23 12:45:43 -0700

    create initial project files

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container center">
+      <h1>Skeleton Angular2 App!</h1>
+      <p>This is also part of the template!</p>
+      <p>Jon said 'always drink upstream from the herd'</p>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
