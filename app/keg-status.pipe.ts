e5c752029b6164616080894943e7e3da7a9d2dda
Aundra Miller <miller.aundra@gmail.com>
2016-03-24 14:12:59 -0700

    create keg-status pipe

---
+++ app/keg-status.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+import { Keg } from './keg.model';
+
+@Pipe ({
+  name: "status",
+  pure: false
+})
+
+export class KegStatusPipe implements PipeTransform {
+  transform(input: Keg[], args) {
+    var kegStatus = args[0];
+    if (kegStatus === "full") {
+      return input.filter(function(keg) {
+        return (keg.pints >= 62);
+      })
+    } else if (kegStatus === "low") {
+      return input.filter(function(keg) {
+        return (keg.pints >= 11);
+      })
+    } else if (kegStatus === "restock") {
+      return input.filter(function(keg) {
+        return (keg.pints <= 10);
+      })
+    }
+  }
+}
