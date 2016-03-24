6a897fa593384042338e5a26a2cbaa2504699063
Aundra Miller <miller.aundra@gmail.com>
2016-03-24 16:15:08 -0700

    add font-weight to kegs of stronger beer

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -8,11 +8,11 @@ import { Keg } from './keg.model';
     <div>
       <h4 class="kegItem"
       [class.expensive]="keg.price > 4.99"
-      [class.empty]="keg.pints === 0">
+      [class.empty]="keg.pints === 0"
+      [class.strong]="keg.percentage > 6.99">
       {{ keg.name }}</h4>
     </div>
   `
-
 })
 
 export class KegComponent {
